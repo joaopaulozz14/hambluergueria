6d27313118a50f5434c0bf2ab57860632e988b38
João Paulo <jp.cm@hotmail.com>
2022-07-06 10:12:27 -0400

    feature: implemented the id verifications of the products, tables and users

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,29 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/service.prisma';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
+import { handleErrorConstraintUnique } from 'src/utils/handle-error-unique.util';
 @Injectable()
 export class ProductsService {
   constructor(private readonly prisma: PrismaService) {}
 
-  create(dto: CreateProductDto): Promise<Product> {
-    return this.prisma.product.create({ data: dto });
+  async create(dto: CreateProductDto): Promise<Product | void> {
+    return this.prisma.product.create({ data: dto }).catch((error) => {
+      throw new UnprocessableEntityException(error.message);
+    });
   }
 
   findAll(): Promise<Product[]> {
     return this.prisma.product.findMany();
   }
+  async verifyIdAndReturnProduct(id: string): Promise<Product> {
+    const product: Product = await this.prisma.product.findUnique({
+      where: { id },
+    });
 
-  findOne(id: string) {
-    return this.prisma.product.findUnique({ where: { id: id } });
+    if (!product) {
+      throw new NotFoundException(`Entrada de id '${id}' não encontrada`);
+    }
+
+    return product;
   }
 
-  update(id: string, dto: UpdateProductDto) {
-    return this.prisma.product.update({ where: { id }, data: dto });
+  findOne(id: string): Promise<Product> {
+    return this.verifyIdAndReturnProduct(id);
   }
 
-  remove(id: string) {
+  async update(id: string, dto: UpdateProductDto) {
+    await this.verifyIdAndReturnProduct(id);
+    return this.prisma.product
+      .update({ where: { id }, data: dto })
+      .catch(handleErrorConstraintUnique);
+  }
+
+  async remove(id: string) {
+    await this.verifyIdAndReturnProduct(id);
     return this.prisma.product.delete({ where: { id } });
   }
 }
--- src/tables/tables.service.ts
+++ src/tables/tables.service.ts
@@ -1,14 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/service.prisma';
 import { CreateTableDto } from './dto/create-table.dto';
 import { UpdateTableDto } from './dto/update-table.dto';
 import { Table } from './entities/table.entity';
+import { handleErrorConstraintUnique } from 'src/utils/handle-error-unique.util';
 @Injectable()
 export class TablesService {
   constructor(private readonly prisma: PrismaService) {}
 
-  create(dto: CreateTableDto): Promise<Table> {
-    return this.prisma.table.create({ data: dto });
+  async create(dto: CreateTableDto): Promise<Table> {
+    return this.prisma.table
+      .create({ data: dto })
+      .catch(handleErrorConstraintUnique);
+  }
+  async verifyIdAndReturnTable(id: string): Promise<Table> {
+    const table: Table = await this.prisma.table.findUnique({ where: { id } });
+
+    if (!table) {
+      throw new NotFoundException(`Entrada de id '${id}' não encontrada`);
+    }
+    return table;
   }
 
   findAll(): Promise<Table[]> {
@@ -16,14 +27,20 @@ export class TablesService {
   }
 
   findOne(id: string) {
-    return this.prisma.table.findUnique({ where: { id } });
+    return this.verifyIdAndReturnTable(id);
   }
 
-  update(id: string, dto: UpdateTableDto) {
-    return this.prisma.table.update({ where: { id }, data: dto });
+  async update(id: string, dto: UpdateTableDto) {
+    await this.verifyIdAndReturnTable(id);
+
+    return this.prisma.table
+      .update({ where: { id }, data: dto })
+      .catch(handleErrorConstraintUnique);
   }
 
-  remove(id: string) {
+  async remove(id: string) {
+    await this.verifyIdAndReturnTable(id);
+
     return this.prisma.table.delete({ where: { id } });
   }
 }
